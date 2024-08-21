e898612490a7f56e46e0a9267de86b2fdf33206c
StephenBuchinskiy <stephenbuchinskiy@outlook.com>
2024-08-21 12:31:17 -0600

    Added count_characters function

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-def readBook(file):
+def read_book(file):
     with open(file) as f:
         return f.read()
 
-def countWords(string):
+def count_words(string):
     words = string.split()
     return len(words)
 
+def count_characters(string):
+    lowered_string = string.lower()
+    dictionary = dict()
+    for character in lowered_string:
+        if character not in dictionary:
+            dictionary[character] = 1
+        else:
+            dictionary[character] += 1
+    
+    return dictionary
+
 def main():
-    string = readBook("books/frankenstein.txt")
-    word_count = countWords(string)
+    string = read_book("books/frankenstein.txt")
+    word_count = count_words(string)
     print(f"{word_count} words were found in the book.")
+    characters = count_characters(string)
+    print(characters)
 
 main()
