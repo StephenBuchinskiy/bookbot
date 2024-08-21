fe61bc8733ace05d887658ea81e22c0b7413145f
StephenBuchinskiy <stephenbuchinskiy@outlook.com>
2024-08-21 12:13:10 -0600

    Added count words function

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-def main():
-    with open("books/frankenstein.txt") as f:
+def readBook(file):
+    with open(file) as f:
         file_contents = f.read()
+    return file_contents
+
+def countWords(string):
+    words = string.split()
+    total_words = 0
+    for word in words:
+        total_words += 1
+    print(total_words)
+
+def main():
+    string = readBook("books/frankenstein.txt")
+    countWords(string)
 
 main()
